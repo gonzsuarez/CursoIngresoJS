8cad6bf30d3234a8c3622b7dc2dd576fc7e059cc
gonzsuarez <78211589+gonzsuarez@users.noreply.github.com>
2021-07-05 20:34:22 -0300

    Add files via upload
    F

---
+++ jsEntradaSalida-04.js
@@ -0,0 +1,12 @@
+/* 
+Debemos lograr tomar un dato por 'PROMPT' 
+y lo muestro por 'ID' al presionar el botón 'mostrar'
+*/
+function mostrar()
+{
+	var nombreIngresado;
+	nombreIngresado=prompt("Ingrese su nombre:");
+	
+	txtIdNombre.value=nombreIngresado;
+}
+
---
+++ jsEntradaSalida-05.js
@@ -0,0 +1,16 @@
+/*
+Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
+ej.: "Usted se llama José y tiene 66 años" 	*/
+function mostrar()
+{	
+	var nombreIngresado;
+	var edadIngresada;
+
+	nombreIngresado=txtIdNombre.value;
+	edadIngresada=txtIdEdad.value;
+
+	alert("Usted se llama: "+nombreIngresado + "y tiene "+ edadIngresada + " años");
+}
+//txtIdNombre
+//txtIdEdad
+
